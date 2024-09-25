25673b6e2799535d4f2dcd14a25f42ecda213527
Nikhil Raj <nr438767@gmail.com>
2024-09-25 08:43:05 +0530

    exploring different http request on same route

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,24 @@ const express= require("express");
 
 const app= express();
 
-app.use((req,res)=>{
-    res.send("hello from server!!!")
+app.get("/user",(req,res)=>{
+    res.send({
+        firstname:"nikhil",
+        lastname:"raj"
+    })
 })
+
+app.post("/user",(req,res)=>{
+    console.log("save data to db");
+    // write logic to save data to db
+    res.send("data saved sucessfully!!");
+})
+
+app.delete("/user",(req,res)=>{
+    // write logic to take data from db and delete it
+    res.send("user deleted successfully!!!");
+})
+
 
 app.listen(3000, ()=>{
     console.log("server up hai aur daur rha");

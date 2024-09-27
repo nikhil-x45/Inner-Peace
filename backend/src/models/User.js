50fd11589ce8a8f9a3f24a2fb33de89e0bc7fcb2
Nikhil Raj <nr438767@gmail.com>
2024-09-28 04:19:59 +0530

    user_model_changes

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -9,6 +9,12 @@ const userSchema= mongoose.Schema({
         lowercase: true,
         trim: true,
     },
+    username:{
+        type:String,
+        unique:true,
+        required:true,
+        trim:true,
+    },
     password:{
         type:String,
         required:true,
